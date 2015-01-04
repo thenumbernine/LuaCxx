0cfdc17e8e9e167421ab1894bb3eee238f60b57b
Christopher E. Moore <christopher.e.moore@gmail.com>
2015-01-04 16:07:45 -0500

    added config for choosing which lua version to use

--- Include.mk
+++ Include.mk
@@ -1,13 +1,20 @@
 LUACXX_PATH:=$(dir $(lastword $(MAKEFILE_LIST)))
+
+include $(LUACXX_PATH)Config.mk
+
 INCLUDE+=$(LUACXX_PATH)include
 DYNAMIC_LIBS+=$(LUACXX_PATH)dist/$(PLATFORM)/$(BUILD)/libLuaCxx.dylib
 
 # Lua 5.2
+ifdef LUACXX_USE_LUA_5_2
 DYNAMIC_LIBS+=/opt/local/lib/liblua.dylib
 INCLUDE+=/opt/local/include
+endif
 
 # LuaJIT 2.0.3
-#DYNAMIC_LIBS+=/usr/local/lib/libluajit-5.1.2.0.3.dylib
-#INCLUDE+=/usr/local/include/luajit-2.0
-#LDFLAGS_osx+=-pagezero_size 10000 -image_base 100000000
+ifdef LUACXX_USE_LUAJIT_2_0_3
+DYNAMIC_LIBS+=/usr/local/lib/libluajit-5.1.2.0.3.dylib
+INCLUDE+=/usr/local/include/luajit-2.0
+LDFLAGS_osx+=-pagezero_size 10000 -image_base 100000000
+endif
 
--- Makefile
+++ Makefile
@@ -3,8 +3,15 @@ DIST_TYPE=lib
 include ../Common/Base.mk
 include ../Tensor/Include.mk
 
+include Config.mk
+
 # Lua 5.2
+ifdef LUACXX_USE_LUA_5_2
 INCLUDE+=/opt/local/include
+endif
 
 # LuaJIT 2.0.3
-#INCLUDE+=/usr/local/include/luajit-2.0
+ifdef LUACXX_USE_LUAJIT_2_0_3
+INCLUDE+=/usr/local/include/luajit-2.0
+endif
+
