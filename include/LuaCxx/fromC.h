319864e3eb0bead2774f4a6cd2d7d7124cbad9d1
Christopher Moore <christopher.e.moore@gmail.com>
2018-07-22 10:51:25 +0200

    partial template specialization...

--- include/LuaCxx/fromC.h
+++ include/LuaCxx/fromC.h
@@ -5,36 +5,55 @@
 namespace LuaCxx {
 
 template<typename T>
-inline void fromC(lua_State* L, T value);
+struct fromC_impl {
+	//static void exec(lua_State* L, const T& value) {}
+};
 
 template<>
-inline void fromC<bool>(lua_State* L, bool value) {
-	lua_pushboolean(L, value);
-}
+struct fromC_impl<bool> {
+	static void exec(lua_State* L, bool value) {
+		lua_pushboolean(L, value);
+	}
+};
 
 template<>
-inline void fromC<int>(lua_State* L, int value) {
-	lua_pushinteger(L, value);
-}
+struct fromC_impl<int> {
+	static void exec(lua_State* L, int value) {
+		lua_pushinteger(L, value);
+	}
+};
 
 template<>
-inline void fromC<float>(lua_State* L, float value) {
-	lua_pushnumber(L, value);
-}
+struct fromC_impl<float> {
+	static void exec(lua_State* L, float value) {
+		lua_pushnumber(L, value);
+	}
+};
 
 template<>
-inline void fromC<double>(lua_State* L, double value) {
-	lua_pushnumber(L, value);
-}
+struct fromC_impl<double> {
+	static void exec(lua_State* L, double value) {
+		lua_pushnumber(L, value);
+	}
+};
 
 template<>
-inline void fromC<const char*>(lua_State* L, const char* value) {
-	lua_pushstring(L, value);
-}
+struct fromC_impl<const char*> {
+	static void exec(lua_State* L, const char* value) {
+		lua_pushstring(L, value);
+	}
+};
 
 template<>
-inline void fromC<std::string>(lua_State* L, std::string value) {
-	lua_pushlstring(L, value.c_str(), value.size());
+struct fromC_impl<std::string> {
+	static void exec(lua_State* L, std::string value) {
+		lua_pushlstring(L, value.c_str(), value.size());
+	}
+};
+
+template<typename T>
+inline void fromC(lua_State* L, const T& value) {
+	fromC_impl<T>::exec(L, value);
 }
 
 }
